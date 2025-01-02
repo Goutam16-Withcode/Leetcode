61ce13e51742dfa687b8c425a7cd96fff017b0ef
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-01-02 17:06:56 +0530

    Time: 3 ms (64.22%) | Memory: 17.6 MB (60.22%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -1,21 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> answerQueries(vector<int>& nums, vector<int>& que) {
+//         int n = nums.size();
+//         int m = que.size();
+//         vector<int> ans(m);
+//         sort(nums.begin(),nums.end());
+//         for(int i=1;i<n;i++){
+//             nums[i]+=nums[i-1];
+//         }
+//         for(int i=0;i<m;i++){
+//             int len = 0;
+//             for(int j=0;j<n;j++){
+//                if(nums[j]>que[i]) break;
+//                len++;
+//             }
+//             ans[i] = len;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& que) {
-        int n = nums.size();
-        int m = que.size();
-        vector<int> ans(m);
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            nums[i]+=nums[i-1];
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        partial_sum(nums.begin(), nums.end(), nums.begin());
+        for (int& q : queries) {
+            q = upper_bound(nums.begin(), nums.end(), q) - nums.begin();
         }
-        for(int i=0;i<m;i++){
-            int len = 0;
-            for(int j=0;j<n;j++){
-               if(nums[j]>que[i]) break;
-               len++;
-            }
-            ans[i] = len;
-        }
-        return ans;
+        return queries;
     }
 };
