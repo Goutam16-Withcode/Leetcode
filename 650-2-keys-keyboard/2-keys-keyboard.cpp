83c4f39129dfaa9813dabd18d91950e4af60b245
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-01-06 00:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (57.11%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+        int minSteps(int n)
+        {
+            if (n == 1)
+                return 0;
+            int min = 100000;
+            int minQuotient = 1;
+            int currentValue = n;
+            int result = 0;
+            while (currentValue != 1)
+            {
+                for (int j = 1; j <= n; ++j)
+                {
+                    if (currentValue % j == 0)
+                    {
+                        auto tmp = currentValue / j;
+                        if (tmp != 1)
+                        {
+                            min = std::min(min, tmp);
+                            minQuotient = j;
+                        }
+                    }
+                }
+                result += min;
+                currentValue = minQuotient;
+                min = 1000;
+            }
+            return result;
+        }
+};
