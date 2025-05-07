e9c2604077406413be926ecbf666eb5925de3c40
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-05-07 23:36:06 +0530

    Time: 36 ms (67.32%) | Memory: 18.6 MB (24.89%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,21 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode*tempA = headA;
-        ListNode*tempB = headB;
-        int lenA = 0;
+        ListNode * tempA = headA;
+        ListNode * tempB = headB;
+        int lenA = 0,lenB = 0;
         while(tempA!=NULL){
             lenA++;
             tempA = tempA->next;
         }
-        int lenB = 0;
         while(tempB!=NULL){
             lenB++;
             tempB = tempB->next;
         }
         tempA = headA;
         tempB = headB;
-        if(lenA>lenB){
+        if(lenA > lenB){
             int diff = lenA - lenB;
             for(int i=1;i<=diff;i++){
                 tempA = tempA->next;
@@ -27,14 +34,14 @@ public:
             return tempA;
         }
         else{
-           int diff = lenB - lenA;
+            int diff = lenB - lenA;
             for(int i=1;i<=diff;i++){
                 tempB = tempB->next;
             }
             while(tempA!=tempB){
                 tempB = tempB->next;
                 tempA = tempA->next;
-            } 
+            }
             return tempB;
         }
     }
