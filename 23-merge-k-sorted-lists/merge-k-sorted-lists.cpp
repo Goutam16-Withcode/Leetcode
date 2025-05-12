62a1c3b39170076fa7a09611fbe32016b3ba49d4
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-05-12 23:26:56 +0530

    Time: 1 ms (75.26%) | Memory: 18.4 MB (65.93%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        vector<int> l;
-        for(int i=0;i<n;i++){
-            ListNode* itr = lists[i];
-            while(itr != NULL){
-                l.push_back(itr->val);
-                itr = itr->next;
+        auto cmp = [](ListNode*a,ListNode*b) {
+            return a->val > b->val ;
+        };
+        priority_queue<ListNode*,vector<ListNode*>,decltype(cmp)>pq(cmp);
+        for(ListNode*node:lists){
+            if(node)pq.push(node);
+        }
+        ListNode*dummy =new ListNode(0);
+        ListNode*tail = dummy;
+        while(!pq.empty()){
+            ListNode*minnode = pq.top();
+            pq.pop();
+            tail->next = minnode;
+            tail = tail->next;
+            if(minnode->next){
+                pq.push(minnode->next);
             }
         }
-        if(n==0 || l.size()==0){
-            return NULL;
-        }
-        sort(l.begin(),l.end());
-        ListNode* head = new ListNode(l[0]);
-        ListNode* it = head;
-        for(int i=1;i<l.size();i++){
-            ListNode* a = new ListNode(l[i]);
-            it->next = a;
-            it = it->next;
-        }
-        it->next = NULL;
-        return head;
+        return dummy->next;
     }
 };
