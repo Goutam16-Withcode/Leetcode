90e94c66596eb01851dbcb5d4ff6f6a2febb2d10
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-05-22 01:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (58.35%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    string toLowerCase(const string& input) {
-        string result = input;
-        transform(result.begin(), result.end(), result.begin(),
-                  [](unsigned char c) { return tolower(c); });
-        return result;
-    }
-
-    // Change from removeNonAlpha to removeNonAlphanumeric
-    string removeNonAlphanumeric(const string& input) {
-        string result = input;
-        result.erase(
-            remove_if(result.begin(), result.end(),
-                      [](unsigned char c) { return !isalnum(c); }),
-            result.end());
-        return result;
-    }
-
     bool isPalindrome(string s) {
-        string mst = toLowerCase(s);
-        string ans = removeNonAlphanumeric(mst);
-        int n = ans.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (ans[i] != ans[n - i - 1]) {
-                return false;
-            }
+        int n = s.length();
+        int left = 0 , right = n-1;
+        while(left < right){
+            if (!isalnum(s[left])) { left++; continue; }
+            if (!isalnum(s[right])) { right--; continue; }
+            if(tolower(s[left])!=tolower(s[right]))  return false;
+            left++,right--;
         }
         return true;
     }
