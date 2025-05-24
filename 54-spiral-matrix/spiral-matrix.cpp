02b1f332c212877b14fb995e2f4b20ba54086562
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-05-24 21:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.00%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -7,28 +7,26 @@ public:
        int left=0, right=n-1, top=0, bottom=m-1;
        int total_ele=m*n;
        vector<int> v;
-       while(count<total_ele){
-                 for(int i=left;i<=right && count<total_ele;i++){
-                    v.push_back(matrix[top][i]);
-                    count++;
+       while(left<=right and top<=bottom){
+                 for(int j=left;j<=right;j++){
+                    v.push_back(matrix[top][j]);
                  }
                  top++;
 
-                  for(int i=top ;i<=bottom && count<total_ele;i++){
+                  for(int i=top ;i<=bottom;i++){
                      v.push_back(matrix[i][right]);
-                     count++;
                   }
                   right--;
-
-                   for(int i=right;i>=left && count<total_ele;i--){
-                    v.push_back(matrix[bottom][i]);
-                    count++;
+                  if(top<=bottom){
+                   for(int j=right;j>=left;j--){
+                    v.push_back(matrix[bottom][j]);
+                   }
                  }
                  bottom--;
-
-                  for(int i=bottom;i>=top && count<total_ele;i--){
+                 if(left<=right){
+                  for(int i=bottom;i>=top;i--){
                     v.push_back(matrix[i][left]);
-                    count++;
+                  }
                  }
                  left++;
        }
