23ffd51e21c7bb55bca66c65000a92293559f78d
Goutam Sharma <goutamshaarma001212@gmail.com>
2025-05-26 22:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (64.24%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    vector<int>dp;
     int integerBreak(int n) {
+        dp.resize(60,-1);
         if(n==2) return 1;
         if(n==3) return 2;
+        if(dp[n]!=-1) return dp[n];
         int product = 1;
         while(n>4){
             product*=3;
             n-=3;
         }
-        return  product*=n;
+        return  dp[n] = product*n;
     }
 };
